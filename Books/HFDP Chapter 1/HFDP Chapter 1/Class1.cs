d81ef9af41cd371f52a8ecb58b29eb6e9207944f
Matthew Knowles <MatthewYKnowles@gmail.com>
2016-03-23 16:17:02 -0400

    initial commit

---
+++ Books/HFDP Chapter 1/HFDP Chapter 1/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFDP_Chapter_1
+{
+    public abstract class Duck
+    {
+        IFlyBehavior flyBehavior;
+        IQuackBehavior quackBehavior;
+        public abstract string performQuack();
+        public abstract string performFly();
+        public abstract string display();
+        public abstract string swim();
+    }
+
+    public class MallardDuck : Duck
+    {
+        public override string performQuack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string performFly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string display()
+        {
+            Console.WriteLine("Mallard Duck");
+        }
+
+        public override string swim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IQuackBehavior
+    {
+    }
+
+    internal interface IFlyBehavior
+    {
+        void fly();
+    }
+
+    public class FlyWithWings : IFlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I can fly");
+        }
+    }
+
+    public class FlyNoWay : IFlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I can't fly");
+        }
+    }
+}
---
+++ PluralSight/BuildingAWebAppShawnWildermuth/TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.NewFolder;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            _mailService.SendMail("",
+                "",
+                $"Contact Page from {model.Name} ({model.Email})",
+                model.Message);
+            return View();
+        }
+    }
+}
