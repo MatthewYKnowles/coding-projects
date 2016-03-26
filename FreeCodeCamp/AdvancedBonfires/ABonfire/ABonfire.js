918d9f7f32d0750af82131d28766eb3958bfdd58
MatthewYKnowles <MatthewYKnowles@gmail.com>
2016-03-26 18:05:56 -0400

    finished Abonfire1

--- FreeCodeCamp/AdvancedBonfires/ABonfire/ABonfire.js
+++ FreeCodeCamp/AdvancedBonfires/ABonfire/ABonfire.js
@@ -1,9 +1,12 @@
+function countryCodeEqualsZero(numbersOnly) {
+  return numbersOnly[0] == 1;
+}
 function telephoneCheck(str) {
   var numbersOnly = str.replace(/-|\s/g, "");
-  if (numbersOnly.length === 13 && numbersOnly[0] == 1 && numbersOnly[1] == "(" && numbersOnly[5] == ")") {return true;}
+  if (str[0] === "-") {return false;}
+  if (numbersOnly.length === 13 && countryCodeEqualsZero(numbersOnly) && numbersOnly[1] == "(" && numbersOnly[5] == ")") {return true;}
+  if (numbersOnly.length === 12 && numbersOnly[0] == "(" && numbersOnly[4] == ")") {return true;}
   if (numbersOnly.length === 11 && numbersOnly[0] == 1) {return true;}
   if (numbersOnly.length === 10) {return true;}
-  else {
-    return false;
-  }
+  else {return false;}
 }
