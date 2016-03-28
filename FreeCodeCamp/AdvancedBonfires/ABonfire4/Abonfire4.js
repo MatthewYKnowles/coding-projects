f3ff80edea41ba0ad7aaf5670262ec5ebd07ddb7
Matthew Knowles <MatthewYKnowles@gmail.com>
2016-03-28 16:34:11 -0400

    finished ABonfire4

--- FreeCodeCamp/AdvancedBonfires/ABonfire4/Abonfire4.js
+++ FreeCodeCamp/AdvancedBonfires/ABonfire4/Abonfire4.js
@@ -1,7 +1,15 @@
 function updateInventory(arr1, arr2) {
     var arrayOneAsObject = inventoryArrayAsObject(arr1, {});
     var arrayPlusNewInventory = inventoryArrayAsObject(arr2, arrayOneAsObject);
-    return inventoryObjectToArray(arrayPlusNewInventory); //need to alphabetize
+    var unsortedArray = inventoryObjectToArray(arrayPlusNewInventory);
+    var sortedArray = unsortedArray.sort(function (a, b) {
+        if (a[0] < b[0])
+            return -1;
+        if (a[0] > b[0])
+            return 1;
+        return 0;
+    });
+    return switchItemsInArrayLocations(sortedArray);
 }
 function inventoryArrayAsObject(arr, obj) {
     var arrayAsObject = obj;
@@ -20,10 +28,20 @@ function inventoryObjectToArray(obj) {
     var objectAsArray = [];
     for (var key in obj) {
         var inventoryItemWithPrice = [];
-        inventoryItemWithPrice.push(obj[key]);
         inventoryItemWithPrice.push(key);
+        inventoryItemWithPrice.push(obj[key]);
         objectAsArray.push(inventoryItemWithPrice);
     }
     return objectAsArray;
+}
+function switchItemsInArrayLocations(arr) {
+    var switchedItemArray = [];
+    for (var i = 0; i < arr.length; i++) {
+        var currentItem = [];
+        currentItem.push(arr[i][1]);
+        currentItem.push(arr[i][0]);
+        switchedItemArray.push(currentItem);
+    }
+    return switchedItemArray;
 }
 //# sourceMappingURL=Abonfire4.js.map
--- FreeCodeCamp/AdvancedBonfires/ABonfire4/Abonfire4.ts
+++ FreeCodeCamp/AdvancedBonfires/ABonfire4/Abonfire4.ts
@@ -1,29 +1,45 @@
- function updateInventory(arr1, arr2) {
-     var arrayOneAsObject = inventoryArrayAsObject(arr1, {});
-     var arrayPlusNewInventory = inventoryArrayAsObject(arr2, arrayOneAsObject);
-     return inventoryObjectToArray(arrayPlusNewInventory); //need to alphabetize
- }
+function updateInventory(arr1, arr2) {
+    var arrayOneAsObject = inventoryArrayAsObject(arr1, {});
+    var arrayPlusNewInventory = inventoryArrayAsObject(arr2, arrayOneAsObject);
+    var unsortedArray = inventoryObjectToArray(arrayPlusNewInventory);
+    var sortedArray = unsortedArray.sort(function (a, b) {
+        if (a[0] < b[0]) return -1;
+        if (a[0] > b[0]) return 1;
+        return 0;
+        });
+    return switchItemsInArrayLocations(sortedArray);
+}
+function inventoryArrayAsObject(arr, obj) {
+    var arrayAsObject = obj;
+    for (var i = 0; i < arr.length; i++) {
+        var currentItem = arr[i][1];
+        if (arrayAsObject[currentItem]) {
+            arrayAsObject[currentItem] += arr[i][0];
+        }
+        else {
+            arrayAsObject[currentItem] = arr[i][0];
+        }
+    }
+    return arrayAsObject;
+}
+function inventoryObjectToArray(obj) {
+    var objectAsArray = [];
+    for (var key in obj) {
+        var inventoryItemWithPrice = [];
+        inventoryItemWithPrice.push(key);
+        inventoryItemWithPrice.push(obj[key]);
+        objectAsArray.push(inventoryItemWithPrice);
+    }
+    return objectAsArray;
+}
 
- function inventoryArrayAsObject(arr, obj){
-     var arrayAsObject = obj;
-     for (var i = 0; i < arr.length; i++){
-         var currentItem = arr[i][1];
-         if (arrayAsObject[currentItem]){
-             arrayAsObject[currentItem] += arr[i][0];
-         } else{
-             arrayAsObject[currentItem] = arr[i][0];   
-         }
-     }
-     return arrayAsObject;
- }
-
- function inventoryObjectToArray(obj){
-     var objectAsArray = [];
-     for (var key in obj){
-         var inventoryItemWithPrice = [];
-         inventoryItemWithPrice.push(obj[key]);
-         inventoryItemWithPrice.push(key);
-         objectAsArray.push(inventoryItemWithPrice);
-     }
-     return objectAsArray;
- }
+function switchItemsInArrayLocations(arr){
+    var switchedItemArray = [];
+    for (var i = 0; i < arr.length; i++){
+        var currentItem = [];
+        currentItem.push(arr[i][1]);
+        currentItem.push(arr[i][0]);
+        switchedItemArray.push(currentItem);
+    }
+    return switchedItemArray;
+}
