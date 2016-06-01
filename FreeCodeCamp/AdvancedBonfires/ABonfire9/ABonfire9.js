941ff14c7b673606b7c0bb5ba8117e5ffc13da52
Matthew Knowles <MatthewYKnowles@gmail.com>
2016-06-01 10:08:42 -0400

    finished Abonfire9

--- FreeCodeCamp/AdvancedBonfires/ABonfire9/ABonfire9.js
+++ FreeCodeCamp/AdvancedBonfires/ABonfire9/ABonfire9.js
@@ -1,14 +1,21 @@
 function pairwise(array, argument) {
   var currentSecondIndex = 1;
+  var alreadyUsedIndexes = []
   var answer = 0;
   for (var i = 0; i < array.length-1; i++){
     for(var j = currentSecondIndex; j < array.length; j++){
-        if (array[i] + array[j] == argument){answer += i+j;}
-        console.log(array[i] + " array i");
-        console.log(array[j] + " array j");
-      //  console.log(answer + " answer");
+        if (array[i] + array[j] == argument && notInArray(i, j, alreadyUsedIndexes)){
+          answer += i+j;
+          alreadyUsedIndexes.push(i);
+          alreadyUsedIndexes.push(j);
+        }
     }
     currentSecondIndex++;
   }
   return answer;
+}
+function notInArray(numberOne, numberTwo, alreadyUsedIndexes){
+  var numberOneInArray = alreadyUsedIndexes.indexOf(numberOne) == -1;
+  var numberTwoInArray = alreadyUsedIndexes.indexOf(numberTwo) == -1;
+  return (numberOneInArray && numberTwoInArray);
 }
