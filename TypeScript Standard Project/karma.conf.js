8e7df8ca1480423d31b4fc91829aa89805f008a1
Matthew Knowles <matthewyknowles@gmail.com>
2016-07-28 17:04:54 -0400

    added code coverage for ts files

--- TypeScript Standard Project/karma.conf.js
+++ TypeScript Standard Project/karma.conf.js
@@ -1,10 +1,23 @@
 module.exports = function(config) {
     config.set({
-        plugins: ['karma-systemjs', 'karma-jasmine', 'karma-chrome-launcher', 'karma-coverage'],
+        plugins: ['karma-systemjs', 'karma-jasmine', 'karma-chrome-launcher', 'karma-remap-istanbul', 'karma-coverage'],
         frameworks: ['systemjs', 'jasmine'],
         files: ['spec/*.js', 'src/*.js'],
-        reporters: ['progress', 'coverage'],
-        preprocessors: {'src/**/*.js': ['coverage']},
+        reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
+        coverageReporter: {
+            type: 'json',
+            subdir: '.',
+            file: 'coverage-final.json'
+        },
+        remapIstanbulReporter: {
+            src: 'coverage/coverage-final.json',
+            reports: {
+                html: 'coverage'
+            },
+            timeoutNotCreated: 1000,
+            timeoutNoMoreFiles: 1000
+        },
+        preprocessors: {'src/*.js': ['coverage']},
         systemjs: {configFile: 'system.config.js'},
         browsers: ['Chrome'],
         singleRun: true
