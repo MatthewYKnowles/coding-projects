0cf1b6ef957b953af48ddd3fafdd5ce4e3aadc09
Matthew Knowles <matthewyknowles@gmail.com>
2016-08-02 17:18:46 -0400

    refactored Poker class to be even smaller

--- Kata/Poker3/src/poker.js
+++ Kata/Poker3/src/poker.js
@@ -6,58 +6,26 @@ var Poker = (function () {
         var hand1 = new Hand(pokerHands.slice(0, 21));
         var hand2 = new Hand(pokerHands.slice(22, 43));
         return hand1.winningRule != hand2.winningRule
-            ? this.differentWinningConditions(hand1, hand2) : this.sameWinningCondition(hand1, hand2, hand1.winningRule);
+            ? this.differentWinningConditions(hand1, hand2) : this.sameWinningConditions(hand1, hand2);
     };
     Poker.differentWinningConditions = function (hand1, hand2) {
-        return this.ruleObject[hand1.winningRule] > this.ruleObject[hand2.winningRule]
+        return this.winningRuleValue(hand1) > this.winningRuleValue(hand2)
             ? hand1.winningString : hand2.winningString;
     };
-    Poker.sameWinningCondition = function (hand1, hand2, winningRule) {
-        return eval(this.winningRuleAsAMethod(winningRule));
-    };
-    Poker.higherPairWins = function (hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.pairValue, hand2.pairValue, hand1, hand2);
-    };
-    Poker.higherFourOfAKindWins = function (hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.fourOfAKindValue, hand2.fourOfAKindValue, hand1, hand2);
-    };
-    Poker.higherThreeOfAKindWins = function (hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.threeOfAKindValue, hand2.threeOfAKindValue, hand1, hand2);
-    };
-    Poker.higherStraitWins = function (hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.straitHighCard, hand2.straitHighCard, hand1, hand2);
-    };
-    Poker.higherStraitFlushWins = function (hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.straitHighCard, hand2.straitHighCard, hand1, hand2);
-    };
-    Poker.higherFullHouseWins = function (hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.threeOfAKindValue, hand2.threeOfAKindValue, hand1, hand2);
-    };
-    Poker.higherFlushWins = function (hand1, hand2) {
-        return this.higherHighCardWins(hand1, hand2);
-    };
-    Poker.higherTwoPairWins = function (hand1, hand2) {
-        if (hand1.pairValue != hand2.pairValue) {
-            return this.higherPairWins(hand1, hand2);
+    Poker.sameWinningConditions = function (hand1, hand2) {
+        if (this.isTwoPair(hand1) && this.firstPairsAreTheSame(hand1, hand2)) {
+            hand1.setWinningCard(hand1.secondPairValue);
+            hand2.setWinningCard(hand2.secondPairValue);
         }
-        var rule = "Two Pair";
-        if (hand1.secondPairValue > hand2.secondPairValue) {
-            hand1.setWinningString(rule.toLowerCase(), hand1.secondPairValue);
+        return this.higherSpecialCardWins(hand1, hand2);
+    };
+    Poker.higherSpecialCardWins = function (hand1, hand2) {
+        if (hand1.winningCard > hand2.winningCard) {
+            hand1.setWinningString(hand1.winningRule, hand1.winningCard);
             return hand1.winningString;
         }
-        if (hand2.secondPairValue > hand1.secondPairValue) {
-            hand2.setWinningString(rule.toLowerCase(), hand2.secondPairValue);
-            return hand2.winningString;
-        }
-        return this.higherHighCardWins(hand1, hand2);
-    };
-    Poker.higherSpecialCardWins = function (hand1Card, hand2Card, hand1, hand2) {
-        if (hand1Card > hand2Card) {
-            hand1.setWinningString(hand1.winningRule.toLowerCase(), hand1Card);
-            return hand1.winningString;
-        }
-        if (hand2Card > hand1Card) {
-            hand2.setWinningString(hand2.winningRule.toLowerCase(), hand2Card);
+        if (hand2.winningCard > hand1.winningCard) {
+            hand2.setWinningString(hand2.winningRule, hand2.winningCard);
             return hand2.winningString;
         }
         return this.higherHighCardWins(hand1, hand2);
@@ -74,11 +42,17 @@ var Poker = (function () {
         }
         return "tie.";
     };
-    Poker.winningRuleAsAMethod = function (winningRule) {
-        return "this.higher" + winningRule.replace(/\s/g, "") + "Wins(hand1, hand2)";
+    Poker.winningRuleValue = function (hand1) {
+        return this.ruleObject[hand1.winningRule];
     };
-    Poker.ruleObject = { "High Card": 0, "Pair": 1, "Two Pair": 2, "Three Of A Kind": 3, "Strait": 4, "Flush": 5,
-        "Full House": 6, "Four Of A Kind": 7, "Strait Flush": 8 };
+    Poker.isTwoPair = function (hand1) {
+        return hand1.winningRule === "two pair";
+    };
+    Poker.firstPairsAreTheSame = function (hand1, hand2) {
+        return hand1.pairValue === hand2.pairValue;
+    };
+    Poker.ruleObject = { "high card": 0, "pair": 1, "two pair": 2, "three of a kind": 3, "strait": 4, "flush": 5,
+        "full house": 6, "four of a kind": 7, "strait flush": 8 };
     return Poker;
 }());
 exports.Poker = Poker;
@@ -87,7 +61,7 @@ var Hand = (function () {
         this.hand = [];
         this.cardValueToNumberObject = { "A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2 };
         this.cardNumberToStringObject = { 14: "Ace", 13: "King", 12: "Queen", 11: "Jack", 10: "Ten", 9: "9", 8: "8", 7: "7", 6: "6", 5: "5", 4: "4", 3: "3", 2: "2" };
-        this.winningRule = "High Card";
+        this.winningRule = "high card";
         this.winningString = "";
         this.winningCard = 0;
         this.highCard = 0;
@@ -103,6 +77,9 @@ var Hand = (function () {
         this.createHandArray(handString.split(" "));
         this.convertFaceCardsToIntegers();
         this.hand.sort(function (a, b) { return b[0] - a[0]; });
+        this.setWinningRule();
+    }
+    Hand.prototype.setWinningRule = function () {
         this.setHighCard();
         this.checkForFlush();
         this.setStraitHighCard();
@@ -117,7 +94,7 @@ var Hand = (function () {
         if (this.flush && this.straitHighCard > 0) {
             this.setStraitFlush();
         }
-    }
+    };
     Hand.prototype.createHandArray = function (hand) {
         for (var i = 0; i < 5; i++) {
             var card = [];
@@ -135,13 +112,14 @@ var Hand = (function () {
         for (var i = 0; i < this.hand.length - 1; i++) {
             if (this.isSecondPair(i)) {
                 this.secondPairValue = this.hand[i][0];
-                this.winningRule = "Two Pair";
-                this.setWinningString(this.winningRule.toLowerCase(), this.pairValue);
+                this.winningRule = "two pair";
+                this.setWinningString(this.winningRule, this.pairValue);
             }
             if (this.isFirstPair(i)) {
                 this.pairValue = this.hand[i][0];
-                this.winningRule = "Pair";
-                this.setWinningString(this.winningRule.toLowerCase(), this.pairValue);
+                this.winningRule = "pair";
+                this.winningCard = this.pairValue;
+                this.setWinningString(this.winningRule, this.pairValue);
             }
         }
     };
@@ -149,16 +127,18 @@ var Hand = (function () {
         for (var i = 0; i < this.hand.length - 2; i++) {
             if (this.containsThreeOfAKind(i)) {
                 this.threeOfAKindValue = this.hand[i][0];
-                this.winningRule = "Three Of A Kind";
-                this.setWinningString(this.winningRule.toLowerCase(), this.threeOfAKindValue);
+                this.winningRule = "three of a kind";
+                this.winningCard = this.threeOfAKindValue;
+                this.setWinningString(this.winningRule, this.threeOfAKindValue);
             }
         }
     };
     Hand.prototype.setFourOfAKindValue = function () {
         if (this.hand[1][0] === this.hand[3][0] && (this.hand[2][0] === this.hand[0][0] || this.hand[2][0] === this.hand[4][0])) {
             this.fourOfAKindValue = this.hand[2][0];
-            this.winningRule = "Four Of A Kind";
-            this.setWinningString(this.winningRule.toLowerCase(), this.fourOfAKindValue);
+            this.winningRule = "four of a kind";
+            this.winningCard = this.fourOfAKindValue;
+            this.setWinningString(this.winningRule, this.fourOfAKindValue);
         }
     };
     Hand.prototype.setStraitHighCard = function () {
@@ -184,8 +164,9 @@ var Hand = (function () {
         }
         if (sameSuit === 5) {
             this.flush = true;
-            this.winningRule = "Flush";
-            this.setWinningString(this.winningRule.toLowerCase(), this.hand[0][0]);
+            this.winningRule = "flush";
+            this.winningCard = this.highCard;
+            this.setWinningString(this.winningRule, this.hand[0][0]);
         }
     };
     Hand.prototype.checkForFullHouse = function () {
@@ -193,23 +174,26 @@ var Hand = (function () {
             for (var i = 0; i < 4; i++) {
                 if (this.areConsecutiveNumbersTheSame(i) && this.hand[i][0] != this.threeOfAKindValue) {
                     this.fullHouse = true;
-                    this.winningRule = "Full House";
-                    this.setWinningString(this.winningRule.toLowerCase(), this.threeOfAKindValue);
+                    this.winningRule = "full house";
+                    this.winningCard = this.threeOfAKindValue;
+                    this.setWinningString(this.winningRule, this.threeOfAKindValue);
                 }
             }
         }
     };
     Hand.prototype.setStraitFlush = function () {
-        this.winningRule = "Strait Flush";
-        this.setWinningString(this.winningRule.toLowerCase(), this.straitHighCard);
+        this.winningRule = "strait flush";
+        this.winningCard = this.straitHighCard;
+        this.setWinningString(this.winningRule, this.straitHighCard);
     };
     Hand.prototype.setStraitAsTheWinningHand = function () {
         this.straitHighCard = this.hand[0][0];
         if (this.isAFiveHighStrait()) {
             this.straitHighCard = 5;
         }
-        this.winningRule = "Strait";
-        this.setWinningString(this.winningRule.toLowerCase(), this.straitHighCard);
+        this.winningRule = "strait";
+        this.winningCard = this.straitHighCard;
+        this.setWinningString(this.winningRule, this.straitHighCard);
     };
     Hand.prototype.containsAStrait = function (consecutiveNumbers) {
         return consecutiveNumbers === 5;
@@ -246,6 +230,9 @@ var Hand = (function () {
     };
     Hand.prototype.areConsecutiveNumbersTheSame = function (i) {
         return this.hand[i][0] === this.hand[i + 1][0];
+    };
+    Hand.prototype.setWinningCard = function (winningCard) {
+        this.winningCard = winningCard;
     };
     Hand.prototype.setHighCard = function () {
         this.highCard = this.hand[0][0];
--- Kata/Poker3/src/poker.ts
+++ Kata/Poker3/src/poker.ts
@@ -1,72 +1,34 @@
 class Poker {
-    static ruleObject: any = {"High Card": 0, "Pair": 1, "Two Pair": 2, "Three Of A Kind": 3, "Strait": 4, "Flush": 5,
-        "Full House": 6, "Four Of A Kind": 7, "Strait Flush": 8};
+    static ruleObject: any = {"high card": 0, "pair": 1, "two pair": 2, "three of a kind": 3, "strait": 4, "flush": 5,
+        "full house": 6, "four of a kind": 7, "strait flush": 8};
 
     static getWinner(pokerHands): string {
         let hand1: Hand = new Hand(pokerHands.slice(0, 21));
         let hand2: Hand = new Hand(pokerHands.slice(22, 43));
         return hand1.winningRule != hand2.winningRule
-            ? this.differentWinningConditions(hand1, hand2) : this.sameWinningCondition(hand1, hand2, hand1.winningRule);
+            ? this.differentWinningConditions(hand1, hand2) : this.sameWinningConditions(hand1, hand2);
     }
 
     static differentWinningConditions(hand1, hand2) {
-        return this.ruleObject[hand1.winningRule] > this.ruleObject[hand2.winningRule]
+        return this.winningRuleValue(hand1) > this.winningRuleValue(hand2)
             ? hand1.winningString : hand2.winningString;
     }
 
-    static sameWinningCondition(hand1, hand2, winningRule) {
-        return eval(this.winningRuleAsAMethod(winningRule));
+    static sameWinningConditions(hand1, hand2) {
+        if (this.isTwoPair(hand1) && this.firstPairsAreTheSame(hand1, hand2)){
+            hand1.setWinningCard(hand1.secondPairValue);
+            hand2.setWinningCard(hand2.secondPairValue);
+        }
+        return this.higherSpecialCardWins(hand1, hand2);
     }
 
-    static higherPairWins(hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.pairValue, hand2.pairValue, hand1, hand2);
-    }
-
-    static higherFourOfAKindWins(hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.fourOfAKindValue, hand2.fourOfAKindValue, hand1, hand2);
-    }
-
-    static higherThreeOfAKindWins(hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.threeOfAKindValue, hand2.threeOfAKindValue, hand1, hand2);
-    }
-
-    private static higherStraitWins(hand1:any, hand2:any) {
-        return this.higherSpecialCardWins(hand1.straitHighCard, hand2.straitHighCard, hand1, hand2);
-    }
-
-    private static higherStraitFlushWins(hand1:any, hand2:any) {
-        return this.higherSpecialCardWins(hand1.straitHighCard, hand2.straitHighCard, hand1, hand2);
-    }
-
-    static higherFullHouseWins(hand1, hand2) {
-        return this.higherSpecialCardWins(hand1.threeOfAKindValue, hand2.threeOfAKindValue, hand1, hand2);
-    }
-
-    static higherFlushWins(hand1, hand2) {
-        return this.higherHighCardWins(hand1, hand2)
-    }
-
-    private static higherTwoPairWins(hand1:any, hand2:any) {
-        if (hand1.pairValue != hand2.pairValue) {return this.higherPairWins(hand1, hand2)}
-        let rule = "Two Pair";
-        if (hand1.secondPairValue> hand2.secondPairValue) {
-            hand1.setWinningString(rule.toLowerCase(), hand1.secondPairValue);
+    static higherSpecialCardWins(hand1, hand2){
+        if (hand1.winningCard > hand2.winningCard) {
+            hand1.setWinningString(hand1.winningRule, hand1.winningCard);
             return hand1.winningString;
         }
-        if (hand2.secondPairValue > hand1.secondPairValue) {
-            hand2.setWinningString(rule.toLowerCase(), hand2.secondPairValue);
-            return hand2.winningString;
-        }
-        return this.higherHighCardWins(hand1, hand2);
-    }
-
-    static higherSpecialCardWins(hand1Card, hand2Card, hand1, hand2){
-        if (hand1Card > hand2Card) {
-            hand1.setWinningString(hand1.winningRule.toLowerCase(), hand1Card);
-            return hand1.winningString;
-        }
-        if (hand2Card > hand1Card) {
-            hand2.setWinningString(hand2.winningRule.toLowerCase(), hand2Card);
+        if (hand2.winningCard > hand1.winningCard) {
+            hand2.setWinningString(hand2.winningRule, hand2.winningCard);
             return hand2.winningString;
         }
         return this.higherHighCardWins(hand1, hand2);
@@ -85,8 +47,16 @@ class Poker {
         return "tie.";
     }
 
-    private static winningRuleAsAMethod(winningRule) {
-        return "this.higher" + winningRule.replace(/\s/g, "") + "Wins(hand1, hand2)";
+    private static winningRuleValue(hand1) {
+        return this.ruleObject[hand1.winningRule];
+    }
+
+    private static isTwoPair(hand1) {
+        return hand1.winningRule === "two pair";
+    }
+
+    private static firstPairsAreTheSame(hand1:any, hand2:any) {
+        return hand1.pairValue === hand2.pairValue;
     }
 }
 
@@ -95,7 +65,7 @@ class Hand {
     hand: any = [];
     cardValueToNumberObject: any = {"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2};
     cardNumberToStringObject: any = {14: "Ace", 13: "King", 12: "Queen", 11: "Jack", 10: "Ten", 9: "9", 8: "8", 7: "7", 6: "6", 5: "5", 4: "4", 3: "3", 2: "2"};
-    winningRule: string = "High Card";
+    winningRule: string = "high card";
     winningString: string = "";
     winningCard: number = 0;
     highCard: number = 0;
@@ -113,6 +83,10 @@ class Hand {
         this.createHandArray(handString.split(" "));
         this.convertFaceCardsToIntegers();
         this.hand.sort(function(a,b) {return b[0] - a[0]});
+        this.setWinningRule();
+    }
+
+    setWinningRule() {
         this.setHighCard();
         this.checkForFlush();
         this.setStraitHighCard();
@@ -138,14 +112,15 @@ class Hand {
         for (let i = 0; i < this.hand.length - 1; i++) {
             if (this.isSecondPair(i)){
                 this.secondPairValue = this.hand[i][0];
-                this.winningRule = "Two Pair";
-                this.setWinningString(this.winningRule.toLowerCase(), this.pairValue);
+                this.winningRule = "two pair";
+                this.setWinningString(this.winningRule, this.pairValue);
             }
 
             if (this.isFirstPair(i)){
                 this.pairValue = this.hand[i][0];
-                this.winningRule = "Pair";
-                this.setWinningString(this.winningRule.toLowerCase(), this.pairValue);
+                this.winningRule = "pair";
+                this.winningCard = this.pairValue;
+                this.setWinningString(this.winningRule, this.pairValue);
             }
         }
     }
@@ -154,8 +129,9 @@ class Hand {
         for (let i = 0; i < this.hand.length - 2; i++) {
             if (this.containsThreeOfAKind(i)){
                 this.threeOfAKindValue = this.hand[i][0];
-                this.winningRule = "Three Of A Kind";
-                this.setWinningString(this.winningRule.toLowerCase(), this.threeOfAKindValue);
+                this.winningRule = "three of a kind";
+                this.winningCard = this.threeOfAKindValue;
+                this.setWinningString(this.winningRule, this.threeOfAKindValue);
             }
         }
     }
@@ -163,8 +139,9 @@ class Hand {
     setFourOfAKindValue() {
         if(this.hand[1][0] === this.hand[3][0] && (this.hand[2][0] === this.hand[0][0] || this.hand[2][0] === this.hand[4][0]) ) {
             this.fourOfAKindValue = this.hand[2][0];
-            this.winningRule = "Four Of A Kind";
-            this.setWinningString(this.winningRule.toLowerCase(), this.fourOfAKindValue);
+            this.winningRule = "four of a kind";
+            this.winningCard = this.fourOfAKindValue;
+            this.setWinningString(this.winningRule, this.fourOfAKindValue);
         }
     }
 
@@ -192,8 +169,9 @@ class Hand {
         }
         if (sameSuit === 5) {
             this.flush = true;
-            this.winningRule = "Flush";
-            this.setWinningString(this.winningRule.toLowerCase(), this.hand[0][0])
+            this.winningRule = "flush";
+            this.winningCard = this.highCard;
+            this.setWinningString(this.winningRule, this.hand[0][0])
         }
     }
 
@@ -202,16 +180,18 @@ class Hand {
             for (let i = 0; i < 4; i++){
                 if (this.areConsecutiveNumbersTheSame(i) && this.hand[i][0] != this.threeOfAKindValue){
                     this.fullHouse = true;
-                    this.winningRule = "Full House";
-                    this.setWinningString(this.winningRule.toLowerCase(), this.threeOfAKindValue);
+                    this.winningRule = "full house";
+                    this.winningCard = this.threeOfAKindValue;
+                    this.setWinningString(this.winningRule, this.threeOfAKindValue);
                 }
             }
         }
     }
 
     setStraitFlush() {
-        this.winningRule = "Strait Flush";
-        this.setWinningString(this.winningRule.toLowerCase(), this.straitHighCard);
+        this.winningRule = "strait flush";
+        this.winningCard = this.straitHighCard;
+        this.setWinningString(this.winningRule, this.straitHighCard);
     }
 
     private setStraitAsTheWinningHand() {
@@ -219,8 +199,9 @@ class Hand {
         if (this.isAFiveHighStrait()) {
             this.straitHighCard = 5;
         }
-        this.winningRule = "Strait";
-        this.setWinningString(this.winningRule.toLowerCase(), this.straitHighCard);
+        this.winningRule = "strait";
+        this.winningCard = this.straitHighCard;
+        this.setWinningString(this.winningRule, this.straitHighCard);
     }
 
     private containsAStrait(consecutiveNumbers:number) {
@@ -261,6 +242,9 @@ class Hand {
     }
     private areConsecutiveNumbersTheSame(i:number) {
         return this.hand[i][0] === this.hand[i + 1][0];
+    }
+    setWinningCard(winningCard) {
+        this.winningCard = winningCard;
     }
 
     private setHighCard() {
