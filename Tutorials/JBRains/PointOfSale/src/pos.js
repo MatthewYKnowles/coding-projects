4625276858f8f30722b657328b274a325830ab09
Matthew Knowles <matthewyknowles@gmail.com>
2016-09-04 16:55:26 -0400

    introduced lookup table

--- Tutorials/JBRains/PointOfSale/src/pos.js
+++ Tutorials/JBRains/PointOfSale/src/pos.js
@@ -16,14 +16,15 @@ var Sale = (function () {
         this._display = display;
     }
     Sale.prototype.onBarcode = function (barcode) {
+        var pricesByBarcode = { "12345": "$7.95", "23456": "$12.50" };
         if (barcode === "") {
             this._display.setText("Scanning error: empty barcode");
         }
         else if (barcode === "12345") {
-            this._display.setText("$7.95");
+            this._display.setText(pricesByBarcode["12345"]);
         }
         else if (barcode === "23456") {
-            this._display.setText("$12.50");
+            this._display.setText(pricesByBarcode["23456"]);
         }
         else {
             this._display.setText("Product not found for " + barcode);
--- Tutorials/JBRains/PointOfSale/src/pos.ts
+++ Tutorials/JBRains/PointOfSale/src/pos.ts
@@ -17,14 +17,15 @@ export class Sale {
         this._display = display;
     }
     onBarcode(barcode: string) {
+        let pricesByBarcode = {"12345": "$7.95", "23456": "$12.50"};
         if (barcode === ""){
             this._display.setText("Scanning error: empty barcode");
         }
         else if (barcode === "12345"){
-            this._display.setText("$7.95");
+            this._display.setText(pricesByBarcode["12345"]);
         }
         else if (barcode === "23456") {
-            this._display.setText("$12.50");
+            this._display.setText(pricesByBarcode["23456"]);
         }
         else {
             this._display.setText("Product not found for " + barcode);
