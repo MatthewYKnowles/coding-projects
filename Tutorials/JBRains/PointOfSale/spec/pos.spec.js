194ea36dbe18e8cd1ec27cc838a29afb80e204cc
Matthew Knowles <matthewyknowles@gmail.com>
2016-09-05 17:45:21 -0400

    added beforeEach to tests

--- Tutorials/JBRains/PointOfSale/spec/pos.spec.js
+++ Tutorials/JBRains/PointOfSale/spec/pos.spec.js
@@ -1,31 +1,27 @@
 "use strict";
 var pos_1 = require("../src/pos");
 describe("Sell One Item", function () {
+    var display;
+    var pricesByBarcode = { "12345": "$7.95", "23456": "$12.50" };
+    var sale;
+    beforeEach(function () {
+        display = new pos_1.Display();
+        sale = new pos_1.Sale(display, pricesByBarcode);
+    });
     it("should find the product", function () {
-        var display = new pos_1.Display();
-        var pricesByBarcode = { "12345": "$7.95", "23456": "$12.50" };
-        var sale = new pos_1.Sale(display, pricesByBarcode);
         sale.onBarcode("12345");
         expect(display.getText()).toBe("$7.95");
     });
     it("should find another product", function () {
-        var display = new pos_1.Display();
-        var pricesByBarcode = { "12345": "$7.95", "23456": "$12.50" };
-        var sale = new pos_1.Sale(display, pricesByBarcode);
         sale.onBarcode("23456");
         expect(display.getText()).toBe("$12.50");
     });
     it("should show product not found", function () {
-        var display = new pos_1.Display();
-        var pricesByBarcode = { "12345": "$7.95", "23456": "$12.50" };
-        var sale = new pos_1.Sale(display, pricesByBarcode);
         sale.onBarcode("99999");
         expect(display.getText()).toBe("Product not found for 99999");
     });
     it("should show empty barcode when no barcode passed in", function () {
-        var display = new pos_1.Display();
-        var pricesByBarcode = { "12345": "$7.95", "23456": "$12.50" };
-        var sale = new pos_1.Sale(display, pricesByBarcode);
+        var sale = new pos_1.Sale(display, null);
         sale.onBarcode("");
         expect(display.getText()).toBe("Scanning error: empty barcode");
     });
--- Tutorials/JBRains/PointOfSale/spec/pos.spec.ts
+++ Tutorials/JBRains/PointOfSale/spec/pos.spec.ts
@@ -1,30 +1,26 @@
 import {Display, Sale} from "../src/pos";
 describe("Sell One Item", ()=> {
+    let display: Display;
+    let pricesByBarcode = {"12345": "$7.95", "23456": "$12.50"};
+    let sale: Sale;
+    beforeEach(()=> {
+        display = new Display();
+        sale = new Sale(display, pricesByBarcode);
+    });
     it("should find the product", ()=> {
-        let display: Display = new Display();
-        let pricesByBarcode = {"12345": "$7.95", "23456": "$12.50"};
-        let sale: Sale = new Sale(display, pricesByBarcode);
         sale.onBarcode("12345");
         expect(display.getText()).toBe("$7.95");
     });
     it("should find another product", ()=> {
-        let display: Display = new Display();
-        let pricesByBarcode = {"12345": "$7.95", "23456": "$12.50"};
-        let sale: Sale = new Sale(display, pricesByBarcode);
         sale.onBarcode("23456");
         expect(display.getText()).toBe("$12.50");
     });
     it("should show product not found", ()=> {
-        let display: Display = new Display();
-        let pricesByBarcode = {"12345": "$7.95", "23456": "$12.50"};
-        let sale: Sale = new Sale(display, pricesByBarcode);
         sale.onBarcode("99999");
         expect(display.getText()).toBe("Product not found for 99999");
     });
     it("should show empty barcode when no barcode passed in", ()=> {
-        let display: Display = new Display();
-        let pricesByBarcode = {"12345": "$7.95", "23456": "$12.50"};
-        let sale: Sale = new Sale(display, pricesByBarcode);
+        let sale: Sale = new Sale(display, null);
         sale.onBarcode("");
         expect(display.getText()).toBe("Scanning error: empty barcode");
     });
