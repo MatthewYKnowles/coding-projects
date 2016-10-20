08091b53a6f462b1ec43a96e9f1d1cb9d12037aa
Matthew Knowles <matthewyknowles@gmail.com>
2016-10-20 08:04:43 -0400

    added MongoDB .Net class to coding projects

---
+++ Tutorials/M101DotNet/M101DotNet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace M101DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] arg)
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase("test");
+            var col = db.GetCollection<BsonDocument>("people");
+        }
+    }
+}
