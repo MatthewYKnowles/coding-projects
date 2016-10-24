f2c36c2f75ab7c3473e2993b4f1001a344d8334e
Matthew Knowles <matthewyknowles@gmail.com>
2016-10-23 20:20:53 -0400

    Finished adding and reading from a database

--- Tutorials/M101DotNet/M101DotNet/Program.cs
+++ Tutorials/M101DotNet/M101DotNet/Program.cs
@@ -20,10 +20,43 @@ namespace M101DotNet
 
         static async Task MainAsync(string[] arg)
         {
-            var connectionString = "mongodb://localhost:27017";
+            var connectionString = "mongodb://localhose:27017";
             var client = new MongoClient(connectionString);
             var db = client.GetDatabase("test");
             var col = db.GetCollection<BsonDocument>("people");
+            //var doc = new Person
+            //{
+            //    Name = "Jones",
+            //    Age = 24,
+            //    Profession = "Hacker"
+            //};
+            //Console.WriteLine(doc.Id);
+            //await col.InsertOneAsync(doc);
+            //Console.WriteLine(doc.Id);
+
+            //using (var cursor = await col.Find(new BsonDocument()).ToCursorAsync())
+            //{
+            //    while (await cursor.MoveNextAsync())
+            //    {
+            //        foreach (var doc in cursor.Current)
+            //        {
+            //         Console.WriteLine(doc);   
+            //        }
+            //    }
+
+            var list = await col.Find(new BsonDocument()).ToListAsync();
+            foreach (var doc in list)
+            {
+                Console.WriteLine(doc);
+            }
         }
+    }
+
+    class Person
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
     }
 }
