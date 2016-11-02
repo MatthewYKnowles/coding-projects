ea241dac8765a708dee4de7b3946a652b3cd82b4
Matthew knowles <matthew.knowles@greatersum.com>
2016-11-02 14:03:38 -0400

    Finished week 3 homework

--- Tutorials/M101DotNet/M101DotNet/Program.cs
+++ Tutorials/M101DotNet/M101DotNet/Program.cs
@@ -20,19 +20,27 @@ namespace M101DotNet
         static async Task MainAsync(string[] args)
         {
             var client = new MongoClient();
-            var db = client.GetDatabase("students");
-            var col = db.GetCollection<BsonDocument>("grades");
-            var list = await col.Find(new BsonDocument("type", "homework"))
-                .Sort(Builders<BsonDocument>.Sort.Ascending("student_id").Ascending("score"))
-                .ToListAsync();
-            var lastStudentId = -1;
+            var db = client.GetDatabase("school");
+            var col = db.GetCollection<BsonDocument>("students");
+            Console.WriteLine(col);
+            var list = await col.Find(new BsonDocument()).ToListAsync();
+
+            //var collection = db.GetCollection<Person>("people");
+            //var filter = new BsonDocument("username", "bodrum");
+            //var update = Builders<Person>.Update.PullFilter("followerList",
+            //    Builders<Follower>.Filter.Eq("follower", "fethiye"));
+            //var result = collection.FindOneAndUpdateAsync(filter, update).Result;
             foreach (var doc in list)
             {
-                if (doc["student_id"] != lastStudentId)
-                {
-                    col.DeleteOne(doc);
-                }
-                lastStudentId = doc["student_id"].ToInt32();
+                var homeworkOneScore = doc["scores"][2];
+                var homeworkTwoScore = doc["scores"][3];
+                var lowHomework = homeworkOneScore <= homeworkTwoScore ? homeworkOneScore : homeworkTwoScore;
+
+                var update = Builders<BsonDocument>.Update.PullFilter("scores",
+                    Builders<BsonDocument>.Filter.Eq("score", lowHomework["score"]));
+
+                var result = await col.FindOneAndUpdateAsync(doc, update);
+                Console.WriteLine(result);
             }
         }
     }
---
+++ Tutorials/M101DotNet/MongoHomework/2.3/src/M101DotNet.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using M101DotNet.WebApp.Models;
+using M101DotNet.WebApp.Models.Account;
+using MongoDB.Bson;
+
+namespace M101DotNet.WebApp.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var blogContext = new BlogContext();
+            var col = blogContext.Users;
+            var filter = Builders<User>.Filter.Eq("Email", model.Email);
+            var list = await col.Find(filter).ToListAsync();
+            User user = list[0];
+
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "Email address has not been registered.");
+                return View(model);
+            }
+
+            var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email)
+                },
+                "ApplicationCookie");
+
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignIn(identity);
+
+            return Redirect(GetRedirectUrl(model.ReturnUrl));
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new RegisterModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User
+            {
+                Email = model.Email,
+                Name = model.Name
+            };
+
+            var blogContext = new BlogContext();
+            var col = blogContext.Users;
+            await col.InsertOneAsync(user);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+    }
+}
