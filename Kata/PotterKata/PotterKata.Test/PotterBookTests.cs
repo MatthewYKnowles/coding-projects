c659cae763ae3b8346b420002979262c37f7d94c
Matthew Knowles <matthewyknowles@gmail.com>
2016-12-11 21:26:52 -0500

    Started Poker Kata

---
+++ Kata/PotterKata/PotterKata.Test/PotterBookTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterKata.Test
+{
+    public class PotterBookTests
+    {
+    }
+}
