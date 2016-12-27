6186af2d2a42d3ef1df4f3808ff7fadb4ac97391
Matthew knowles <matthew.knowles@greatersum.com>
2016-12-27 12:15:35 -0500

    Started PotterKata2

---
+++ Kata/PotterKatas/PotterKata2/PotterKata2/PotterBooksTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal.Filters;
+
+namespace PotterKata2
+{
+    [TestFixture]
+    public class PotterBooksTests
+    {
+        [TestCase(new[] {0}, ExpectedResult = 8)]
+        [TestCase(new[] {0, 1}, ExpectedResult = 8 * 2)]
+        [TestCase(new[] {0, 1, 2}, ExpectedResult = 8 * 3)]
+        public decimal OneBook(int[] books)
+        {
+            return PotterBooks.Price(books);
+        }
+    }
+
+    public class PotterBooks
+    {
+        public static decimal Price(int[] books)
+        {
+            return books.Length * 8;
+        }
+    }
+}
