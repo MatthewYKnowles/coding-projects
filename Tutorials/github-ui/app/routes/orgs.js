4993d153918c6fd3b0cc3310d48c793ddc8bdfb8
Matthew Knowles <matthewyknowles@gmail.com>
2017-02-17 19:24:00 -0500

    Finished exercise 7

--- Tutorials/github-ui/app/routes/orgs.js
+++ Tutorials/github-ui/app/routes/orgs.js
@@ -4,7 +4,14 @@ export default Ember.Route.extend({
     favorites: Ember.inject.service(),
     actions: {
         favoriteClicked(org) {
-            this.get('favorites').favoriteItem(org);
+            if (this.get('favorites.items').indexOf(org) < 0){
+                this.get('favorites').favoriteItem(org);
+            } else {
+                this.get('favorites').unfavoriteItem(org);
+            }
+        },
+        linksToggled() {
+            console.log("Toggled");
         }
     },
     model() {
--- Tutorials/github-ui/app/services/favorites.js
+++ Tutorials/github-ui/app/services/favorites.js
@@ -4,6 +4,8 @@ export default Ember.Service.extend({
     items: [],
     favoriteItem(item) {
         this.get('items').addObject(item);
-        console.log(this.get('items').map(x => x.id).join(', '));
+    },
+    unfavoriteItem(item) {
+        this.get('items').removeObject(item);
     }
 });
