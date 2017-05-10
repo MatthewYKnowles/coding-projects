bb87e5e8721ab2341262a4ed3e5a8a5245e7bb38
Matthew Knowles <matthewyknowles@gmail.com>
2017-05-10 15:30:48 -0400

    Bob and Leap Exercism exercises

--- Exercism/csharp/bob/Bob.cs
+++ Exercism/csharp/bob/Bob.cs
@@ -1,9 +1,50 @@
-using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Hey(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return AngryResponse(statement)
+            ? "Whoa, chill out!"
+            : ShouldGiveCalmResponse(statement);
+    }
+
+    private static string ShouldGiveCalmResponse(string statement)
+    {
+        string statementWithoutSpaces = statement.Replace(" ", "");
+        if (Empty(statementWithoutSpaces))
+        {
+            return "Fine. Be that way!";
+        }
+        if (IsAQuestion(statementWithoutSpaces))
+        {
+            return "Sure.";
+        }
+        return "Whatever.";
+    }
+
+    private static bool Empty(string statementWithoutSpaces)
+    {
+        return statementWithoutSpaces == "";
+    }
+
+    private static bool IsAQuestion(string statementWithoutSpaces)
+    {
+        return statementWithoutSpaces.EndsWith("?");
+    }
+
+    private static bool AngryResponse(string statement)
+    {
+        return HasAnyLetters(statement) && AllLettersAreCapitalized(statement);
+    }
+
+    private static bool AllLettersAreCapitalized(string statement)
+    {
+        return statement.ToUpper() == statement;
+    }
+
+    private static bool HasAnyLetters(string statement)
+    {
+        return statement.Any(char.IsLetter);
     }
 }
