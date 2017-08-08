49457199f8a47ea3b0c249c18ebcd23bb369304a
Matthew Knowles <MatthewYKnowles@gmail.com>
2017-08-08 09:24:26 -0400

    Refactor Java Exercism Pangram

--- Exercism/java/pangram/src/main/java/PangramChecker.java
+++ Exercism/java/pangram/src/main/java/PangramChecker.java
@@ -2,21 +2,16 @@ class PangramChecker {
 
     boolean isPangram(String sentence) {
         String formattedSentence = removeSpecialCharactersAndMakeLowerCase(sentence);
-        Alphabet pangramData = new Alphabet();
-        return pangramData.LettersOfAlphabetNotUsedBySentence(formattedSentence).length() == 0;
+        return lettersOfAlphabetNotUsedBySentence(formattedSentence).length() == 0;
     }
 
     private String removeSpecialCharactersAndMakeLowerCase(String input) {
         return input.replaceAll("[^A-Za-z]", "").toLowerCase();
     }
-}
 
-class Alphabet {
-
-    private String lettersInAlphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    String LettersOfAlphabetNotUsedBySentence(String sentence) {
-        while (sentenceOrAlphabetStillHaveLetters(sentence)) {
+    private String lettersOfAlphabetNotUsedBySentence(String sentence) {
+        String lettersInAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        while (sentenceOrAlphabetStillHaveLetters(sentence, lettersInAlphabet)) {
             String currentLetter = String.valueOf(sentence.charAt(0));
             lettersInAlphabet = lettersInAlphabet.replace(currentLetter, "");
             sentence = sentence.replace(currentLetter, "");
@@ -24,7 +19,7 @@ class Alphabet {
         return lettersInAlphabet;
     }
 
-    private boolean sentenceOrAlphabetStillHaveLetters(String formattedSentence) {
-        return lettersInAlphabet.length() > 0 && formattedSentence.length() > 0;
+    private boolean sentenceOrAlphabetStillHaveLetters(String sentenceRemaining, String alphabetRemaining) {
+        return alphabetRemaining.length() > 0 && sentenceRemaining.length() > 0;
     }
 }
