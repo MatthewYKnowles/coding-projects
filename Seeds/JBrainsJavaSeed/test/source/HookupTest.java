3f88f17bc19548add101aa6935215ea7525707e7
Matthew Knowles <MatthewYKnowles@gmail.com>
2017-08-10 11:14:16 -0400

    Finished JB Rains Java Seed

---
+++ Seeds/JBrainsJavaSeed/test/source/HookupTest.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HookupTest {
+    @Test
+    public void isThis() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}
