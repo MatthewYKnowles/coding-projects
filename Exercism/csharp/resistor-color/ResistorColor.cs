ef6a70e5810abfa475d32f3d21b932fdf26b74ce
MatthewYKnowles <matthewyknowles@gmail.com>
2019-06-06 11:29:48 -0400

    started resistor color exercism

---
+++ Exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        return 0;
+    }
+
+    public static string[] Colors()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
