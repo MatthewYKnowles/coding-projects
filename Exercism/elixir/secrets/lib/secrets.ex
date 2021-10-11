02efbfabed9acb68d8cb488dad74d2844ae4daca
Matthew Knowles <matthewyknowles@gmail.com>
2021-10-11 06:51:35 -0400

    some Exercism Elixir projects

---
+++ Exercism/elixir/secrets/lib/secrets.ex
@@ -0,0 +1,21 @@
+defmodule Secrets do
+  def secret_add(secret), do: fn x -> x + secret end
+
+  def secret_subtract(secret), do: fn x -> x - secret end
+
+  def secret_multiply(secret), do: fn x -> x * secret end
+
+  def secret_divide(secret), do: fn x -> x / secret end
+
+  def secret_and(secret) do
+    # Please implement the secret_and/1 function
+  end
+
+  def secret_xor(secret) do
+    # Please implement the secret_xor/1 function
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    # Please implement the secret_combine/2 function
+  end
+end
