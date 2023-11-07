3786a5e9ea3a2f76e335a72f4262d96b91a20222
Matthew Knowles <matthewyknowles@gmail.com>
2023-11-07 15:12:44 -0500

    passing test

--- Talks/Who Watches the Watchmen in PHP/tests/MovieRentalStoreTest.php
+++ Talks/Who Watches the Watchmen in PHP/tests/MovieRentalStoreTest.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 
-class MovieRentalStoreTest extends TestCase
-{
+class MovieRentalStoreTest extends TestCase {
+    private $classUnderTest;
+    protected function setUp(): void
+    {
+        $this->classUnderTest = new MovieRentalStore();
+    }
+
     public function testOne(): void {
-        $movieRentalStore = new MovieRentalStore();
-        static::assertSame(15, $movieRentalStore->rental(15));
+        $properResult =
+            "Rental Record for martin\n\tRan\t5\n\tTrois Couleurs: Bleu\t2\nAmount owed is 7\nYou earned 2 frequent renter points\n";
+        $customer = (object)[
+            'name' => 'martin',
+            'rentals' => [(object)['movieID'=> 'F001', 'days' => 3 ], (object)[ 'movieID' => 'F002', 'days' => 1 ]]
+    ];
+        $actualResult = $this->classUnderTest->getInfo($customer);
+        static::assertSame($properResult, $actualResult);
     }
 }
