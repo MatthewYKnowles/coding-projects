d52f33b7d13d43a3132c71efebd43fa755b08d3c
Matthew Knowles <matthewyknowles@gmail.com>
2023-11-07 15:45:36 -0500

    reformatted test

--- Talks/Who Watches the Watchmen in PHP/tests/MovieRentalStoreTest.php
+++ Talks/Who Watches the Watchmen in PHP/tests/MovieRentalStoreTest.php
@@ -13,7 +13,11 @@ class MovieRentalStoreTest extends TestCase {
 
     public function testOne(): void {
         $properResult =
-            "Rental Record for martin\n\tRan\t5\n\tTrois Couleurs: Bleu\t2\nAmount owed is 7\nYou earned 2 frequent renter points\n";
+            "Rental Record for martin\n" .
+                "\tRan\t5\n" .
+                "\tTrois Couleurs: Bleu\t2\n" .
+            "Amount owed is 7\n" .
+            "You earned 2 frequent renter points\n";
         $customer = (object)[
             'name' => 'martin',
             'rentals' => [(object)['movieID'=> 'F001', 'days' => 3 ], (object)[ 'movieID' => 'F002', 'days' => 1 ]]
